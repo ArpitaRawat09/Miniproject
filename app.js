01e811ade68995d940f689acad993e514fc01e5c
Arpita Rawat <arpitarawat117@gamil.com>
2024-09-24 13:44:42 +0530

    backend learn

---
+++ app.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const app = express();
+
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/profile", isLoggedIn, async (req, res) => {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("posts");
+  // console.log(req.user);
+  res.render("profile", { user });
+});
+
+app.post("/post", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  let { content } = req.body;
+  let post = await postModel.create({
+    user: user._id,
+    content,
+  });
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
+});
+
+app.post("/register", async (req, res) => {
+  let { email, username, password, age, name } = req.body;
+
+  let user = await userModel.findOne({ email });
+  if (user) {
+    return res.status(500).send("user already registered");
+  }
+
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, async function (err, hash) {
+      let user = await userModel.create({
+        username,
+        email,
+        name,
+        age,
+        password: hash,
+      });
+      let token = jwt.sign({ email: email, userid: user._id }, "shhhhhhhhh");
+      res.cookie("token", token);
+      res.send("registered");
+      // console.log(hash);
+    });
+    // console.log(salt);
+  });
+
+  // res.render("index");
+});
+
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+
+  let user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(500).send("Something went wrong");
+  }
+  bcrypt.compare(password, user.password, function (err, result) {
+    // result == true
+    if (result) {
+      let token = jwt.sign({ email: user.email }, "shhhhhhhhh");
+      res.cookie("token", token);
+      res.status(200).redirect("/profile");
+    } else res.redirect("/login");
+  });
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/login");
+});
+
+// middleware
+
+function isLoggedIn(req, res, next) {
+  if (req.cookies.token === "") res.redirect("/login");
+  else {
+    let data = jwt.verify(req.cookies.token, "shhhhhhhhh");
+    req.user = data;
+    next();
+  }
+}
+
+app.listen(3000);
